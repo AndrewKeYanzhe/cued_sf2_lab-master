6609988d1dcca9635baa8074156ec2ae341fd4fd
Andrew <keyanzhe@gmail.com>
2024-06-07 22:29:16 +0100

    compare.py 1 level dct

--- compare.py
+++ compare.py
@@ -4,6 +4,9 @@ import numpy as np
 # Data from the table
 categories = ['lighthouse', 'bridge', 'flamingo', '2024 image']
 
+rms_1layer = [8.65, 12.9, 11.8, 6.41]
+ssim_1layer = [0.848, 0.764, 0.847, 0.922]
+
 rms_2layer = [8.01, 12.56, 11.3, 6.09]
 ssim_2layer = [0.868, 0.779, 0.859, 0.93]
 
@@ -15,23 +18,25 @@ x = np.arange(len(categories))  # the label locations
 fig, (ax1, ax2) = plt.subplots(1, 2, figsize=(8, 4))
 
 # Plotting RMS error
+ax1.plot(x, rms_1layer, 'o-', label='1-level DCT')
 ax1.plot(x, rms_2layer, 'o-', label='2-level DCT')
-ax1.plot(x, rms_2layer_deblock, 's-', label='2-level DCT + deblock')
+ax1.plot(x, rms_2layer_deblock, 'o-', label='2-level DCT + deblock')
 
 ax1.set_xlabel('Image')
 ax1.set_ylabel('RMS error')
-ax1.set_title('RMS error by category and method')
+ax1.set_title('RMS error')
 ax1.set_xticks(x)
 ax1.set_xticklabels(categories)
 ax1.legend()
 
 # Plotting SSIM
+ax2.plot(x, ssim_1layer, 'o-', label='1-level DCT')
 ax2.plot(x, ssim_2layer, 'o-', label='2-level DCT')
-ax2.plot(x, ssim_2layer_deblock, 's-', label='2-level DCT + deblock')
+ax2.plot(x, ssim_2layer_deblock, 'o-', label='2-level DCT + deblock')
 
-ax2.set_xlabel('Categories')
-ax2.set_ylabel('Image')
-ax2.set_title('SSIM by category and method')
+ax2.set_xlabel('Image')
+ax2.set_ylabel('SSIM')
+ax2.set_title('SSIM')
 ax2.set_xticks(x)
 ax2.set_xticklabels(categories)
 ax2.legend()
